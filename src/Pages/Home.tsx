d5c8e72a5104042d6cb5f55aaf5dce65b124bdf9
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-05-06 21:05:07 -0300

    css update

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,25 @@
+import {getDocs, collection} from 'firebase/firestore'
+import { db } from '../config/firebase'
+import { useState } from 'react'
+
+interface Post {
+  id: string,
+  userId: string,
+  title: string,
+  username: string,
+  description: string,
+}
+
 export const Home = () => {
+  const [postsList, setPostsList] = useState<Post[] | null>(null)
+  const postsRef = collection(db, "posts")
+
+  const getPosts = async () => {
+    const data = await getDocs(postsRef)
+    setPostsList(data.docs.map((doc) => ({...doc.data(), id: doc.id})) as Post[])
+  }
   
+  getPosts()
   return (
     <div>
       <h1>HOMEPAGE</h1>
